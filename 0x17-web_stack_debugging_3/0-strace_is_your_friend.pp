562dc44c5f2da5502f2109121449dc3f03ae9783
sintayehu-244 <101401891+sintayehu-244@users.noreply.github.com>
2022-11-29 19:06:38 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Creat a manifest that fix all termintion of phpp.
-
-exec { 'fix_phpp':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+# Fixes a filename extension typo
+exec { 'rename file':
+  command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
 }
